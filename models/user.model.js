f52b87bc4ca5df3dd4b928eff7a9d5f99b3727d6
pratikShejwal <shejwalp147@gmail.com>
2026-06-09 21:53:36 +0530

    defined user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,14 @@
 // import { uuid } from 'drizzle-orm/gel-core'
-import {pgTable,uuid} from 'drizzle-orm/pg-core'
+// import { timeStamp } from 'console'
+import {pgTable,uuid,timestamp,text,varchar} from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users',{
 id: uuid().primaryKey().defaultRandom(),
-
+firstname:varchar('first_name',{length:255}).notNull(),
+lastname:varchar('last_name',{length:255}),
+email:varchar('email',{length:255}).notNull().unique(),
+password: text().notNull(),
+salt: text().notNull(),
+createdAt: timestamp('created_at').notNull(),
+updatedAt: timestamp('updated_At').$onUpdate(()=>new Date())
 })
