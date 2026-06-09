6f7bdafb21479c135419aa36d09fa5ab6357a9e3
pratikShejwal <shejwalp147@gmail.com>
2026-06-09 20:14:33 +0530

    setting up postgres

---
+++ models/user.model.js
@@ -0,0 +1,7 @@
+// import { uuid } from 'drizzle-orm/gel-core'
+import {pgTable,uuid} from 'drizzle-orm/pg-core'
+
+export const usersTable = pgTable('users',{
+id: uuid().primaryKey().defaultRandom(),
+
+})
