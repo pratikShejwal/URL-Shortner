e5c000f626311e5eb73f798fcdd880a265d94e34
pratikShejwal <shejwalp147@gmail.com>
2026-06-12 23:41:13 +0530

    validations using zod

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 const PORT = process.env.PORT ?? 8000
-
+app.use(express.json())
 app.get('/',(req,res)=>{
     return res.json({message: 'server is running'})
 })
