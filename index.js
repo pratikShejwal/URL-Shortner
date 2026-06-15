3c420064a79f8445d732f63ab31cc043e8d58fbc
pratikShejwal <shejwalp147@gmail.com>
2026-06-15 21:43:09 +0530

    code refactoring

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import userRouter from './routes/user.routes.js'
 const app = express()
-const PORT = process.env.PORT ?? 8000
+const PORT = process.env.PORT ?? 8000;
 app.use(express.json())
+
 app.get('/',(req,res)=>{
     return res.json({message: 'server is running'})
 })
-
 
 app.use('/user',userRouter)
 
